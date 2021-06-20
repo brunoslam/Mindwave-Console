acd3a1cfe8f29872c4e26181653b6c4cc4f1ca7e
Bruno Palma Ávila <bpalma@latinshare.com>
2021-06-20 16:25:44 -0400

    Add project files.

---
+++ Mindwave-Console/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Mindwave_Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
